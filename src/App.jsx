d59af116f72dfb36c76abb385db4e7312e8d2af2
fadillaiizzar <fadillaiizzarm@gmail.com>
2024-11-27 22:36:40 +0700

    revisi twoo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Beranda from "./components/Layouts/Beranda";
 
 function App() {
   return (
-    <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-teal-400 to-gray-800bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#fdf8f2] via-[#f7e4c9] to-[#e7ba86] font-poppins">
+    <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#fdf8f2] via-[#f7e4c9] to-[#e7ba86] font-poppins">
       <Beranda />
       <ProfileSection />
     </div>
